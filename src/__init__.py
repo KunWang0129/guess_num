01ecc21b3d742da5251c4645c9cf46a9e900d810
Kun Wang <kunwang1227@Kuns-MacBook-Pro.local>
2025-09-28 17:14:53 -0400

    src init

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+RL Decomposition for Number Guessing Game
+
+A reinforcement learning environment and agent implementation
+for learning sequence guessing with hierarchical action decomposition.
+"""
